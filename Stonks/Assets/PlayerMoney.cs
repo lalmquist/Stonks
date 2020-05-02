cedb6140f67adb13e6f2dae5c385a9d4de170a10
lalmquist <43098342+lalmquist@users.noreply.github.com>
2020-05-02 11:34:54 -0700

    init

---
+++ Stonks/Assets/PlayerMoney.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerMoney : MonoBehaviour
+{
+    [SerializeField] int playerMoney = 0;
+    [SerializeField] TextMeshProUGUI moneyText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moneyText.text = playerMoney.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyText.text = playerMoney.ToString();
+    }
+}

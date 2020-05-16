ef854f3430b21ab0ba4c6689b187738ee1a09d95
lalmquist <43098342+lalmquist@users.noreply.github.com>
2020-05-16 13:17:03 -0700

    add more scenes

---
+++ Stonks/Assets/Scenes/MainMenu/TradingButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TradingButton : MonoBehaviour
+{
+    public void LoadTradingScene()
+    {
+        SceneManager.LoadScene("Trading");
+    }
+}
